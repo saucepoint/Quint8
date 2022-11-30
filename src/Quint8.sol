f7d2561d859049602ba99178ea0bf2f91953817e
saucepoint <saucepoint@protonmail.com>
2022-11-30 11:08:30 -0500

    checkpointing improvements and tests

--- src/Quint8.sol
+++ src/Quint8.sol
@@ -3,29 +3,29 @@ pragma solidity >=0.8.11;
 
 
 struct Queue {
-    bytes32[4] pages;
     uint128 count;
     uint128 head;
+    bytes32[4] pages;
 }
 
-
 library Quint8 {
-    // 4 pages holding 32x uint8 each, 1024x uint8 total
-    uint256 constant private MAX_CAPACITY = 1024;
-    uint256 constant private PAGE_SIZE = 32;  // 32x uint8 per page
+    // 4 pages holding 32x uint8 each, 128x uint8 total
+    uint128 constant private MAX_CAPACITY = 128;
+    uint128 constant private PAGE_SIZE = 32;  // 32x uint8 per page
 
     function enqueue(Queue memory _queue, uint8 num) internal pure returns (Queue memory) {
         uint256 count = _queue.count;
         require(count < MAX_CAPACITY, "Queue is full");
-
+        
+        uint256 head = _queue.head;
+        
+        // which page to write to (the index of queue.pages)
         uint256 pageIndex;
         unchecked {
-            pageIndex = count / PAGE_SIZE;
+            pageIndex = ((head + count) % MAX_CAPACITY) / PAGE_SIZE;
         }
-        
         bytes32 page = _queue.pages[pageIndex];
 
-        bytes32 insertion;
         assembly {
             // shift the number to the left to pack it into the page
             // ```
@@ -35,13 +35,17 @@ library Quint8 {
             // page = 0x11_22_33_44_55_00_..._00 // insert via OR
             // ```
 
+            // the index to insert *within* a page:
+            // (head + count) % MAX_CAPACITY % PAGE_SIZE
+            let indexInPage := mod(mod(add(head, count), MAX_CAPACITY), PAGE_SIZE)
+            
             // num << ((31 - index) * 8) // (inserting at index=0 means we should left shift by 248 bits)
-            let index := mod(count, PAGE_SIZE)
-            insertion := shl(mul(sub(0x1F, index), 8), num)
+            let insertion := shl(mul(sub(31, indexInPage), 8), num)
             
             // pack the number `insertion` into the page
             page := or(page, insertion)
         }
+        
         _queue.pages[pageIndex] = page;
 
         unchecked { ++_queue.count; }
@@ -49,10 +53,12 @@ library Quint8 {
     }
 
     function dequeue(Queue memory _queue) internal pure returns (Queue memory, uint8) {
+        require(0 < _queue.count, "Queue is empty");
+        
         uint256 head = _queue.head;
         uint256 pageIndex;
         unchecked {
-            pageIndex = _queue.head / 32;
+            pageIndex = head / PAGE_SIZE;
         }
 
         bytes32 page = _queue.pages[pageIndex];
@@ -60,17 +66,18 @@ library Quint8 {
         
         assembly {
             // index of the head pointer, within the page
-            let index := mod(head, 32)
+            let index := mod(head, PAGE_SIZE)
 
             // shift the number to the right to unpack it from the page
             result := shr(mul(sub(0x1F, index), 8), page)
 
             // clear the number from the page
-            page := and(page, not(shl(mul(sub(0x1F, index), 8), 0xFF)))
+            page := and(page, not(shl(mul(sub(31, index), 8), 0xFF)))
         }
+        
         _queue.pages[pageIndex] = page;
         unchecked {
-            ++_queue.head;
+            _queue.head = (_queue.head + 1) % MAX_CAPACITY;
             --_queue.count;
         }
         return (_queue, result);
--- test/Quint8.t.sol
+++ test/Quint8.t.sol
@@ -41,8 +41,93 @@ contract Quint8Test is PRBTest {
         assertEq(q1.dequeue(), 1); //at this point the queue is empty
     }
 
+    function testCannotDequeueWhenStoreIsEmpty1() public {
+        vm.expectRevert();
+        q1.dequeue();
+    }
+
+    function testCannotDequeueWhenStoreIsEmpty2() public {
+        q1.enqueue(5);
+        q1.enqueue(4);
+        q1.enqueue(3);
+        q1.enqueue(2);
+        q1.enqueue(1);
+
+        q1.dequeue();
+        q1.dequeue();
+        q1.dequeue();
+        q1.dequeue();
+        q1.dequeue(); //at this point the queue is empty
+
+        vm.expectRevert();
+        q1.dequeue();
+    }
+
+    // ----------------------------------
+    // ADDITIONAL TESTS
+    // ----------------------------------
+
     function testFive() public {
         q1.yoUhhHmm();
         assertEq(q1.size(), 0);
+    }
+
+    function testFullPageExact() public {
+        for (uint8 i = 0; i < 32; i++) {
+            q1.enqueue(i);
+        }
+        assertEq(q1.size(), 32);
+        for (uint8 i = 0; i < 32; i++) {
+            assertEq(q1.dequeue(), i);
+        }
+        assertEq(q1.size(), 0);
+    }
+
+    function testFullQueueExact() public {
+        for (uint8 i = 0; i < 128; i++) {
+            q1.enqueue(i);
+        }
+        assertEq(q1.size(), 128);
+        for (uint8 i = 0; i < 128; i++) {
+            assertEq(q1.dequeue(), i);
+        }
+        assertEq(q1.size(), 0);
+    }
+
+    function testTwoPageExact() public {
+        for (uint8 i = 0; i < 64; i++) {
+            q1.enqueue(i);
+        }
+        assertEq(q1.size(), 64);
+        for (uint8 i = 0; i < 64; i++) {
+            assertEq(q1.dequeue(), i);
+        }
+        assertEq(q1.size(), 0);
+    }
+
+    function testFifty() public {
+        for (uint8 i = 0; i < 50; i++) {
+            q1.enqueue(i);
+        }
+        assertEq(q1.size(), 50);
+        for (uint8 i = 0; i < 50; i++) {
+            assertEq(q1.dequeue(), i);
+        }
+        assertEq(q1.size(), 0);
+    }
+
+    function testWrapAround() public {
+        uint256 loops = 200;
+        for (uint8 i = 0; i < loops; i++) {
+            q1.enqueue(i);
+            if (i % 2 == 0) {
+                q1.dequeue();
+            }
+        }
+        uint256 half = loops / 2;
+        assertEq(q1.size(), half);
+        for (uint8 i = 0; i < half; i++) {
+            assertEq(q1.dequeue(), (i + half));
+        }
     }
 }
