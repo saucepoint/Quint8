e3b018f1535b3e7be47eb3f242ebddd29990f863
saucepoint <saucepoint@protonmail.com>
2022-11-29 23:41:23 -0500

    checkpointing

--- src/Quint8.sol
+++ src/Quint8.sol
@@ -1,21 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.8.11;
 
-/// max capacity 1024 uint8s
+
 struct Queue {
     bytes32[4] pages;
-    uint128 head;
     uint128 count;
+    uint128 head;
 }
 
+
 library Quint8 {
+    // 4 pages holding 32x uint8 each, 1024x uint8 total
     uint256 constant private MAX_CAPACITY = 1024;
+    uint256 constant private PAGE_SIZE = 32;  // 32x uint8 per page
 
     function enqueue(Queue memory _queue, uint8 num) internal pure returns (Queue memory) {
         uint256 count = _queue.count;
         require(count < MAX_CAPACITY, "Queue is full");
+
+        uint256 pageIndex;
+        unchecked {
+            pageIndex = count / PAGE_SIZE;
+        }
         
-        bytes32 page = _queue.pages[count / 32];
+        bytes32 page = _queue.pages[pageIndex];
 
         bytes32 insertion;
         assembly {
@@ -28,19 +36,21 @@ library Quint8 {
             // ```
 
             // num << ((31 - index) * 8) // (inserting at index=0 means we should left shift by 248 bits)
-            let index := mod(count, 32)
+            let index := mod(count, PAGE_SIZE)
             insertion := shl(mul(sub(0x1F, index), 8), num)
+            
+            // pack the number `insertion` into the page
             page := or(page, insertion)
         }
-        _queue.pages[count / 32] = page;
+        _queue.pages[pageIndex] = page;
 
         unchecked { ++_queue.count; }
         return _queue;
     }
 
     function dequeue(Queue memory _queue) internal pure returns (Queue memory, uint8) {
-        uint128 head = _queue.head;
-        uint128 pageIndex;
+        uint256 head = _queue.head;
+        uint256 pageIndex;
         unchecked {
             pageIndex = _queue.head / 32;
         }
@@ -49,12 +59,16 @@ library Quint8 {
         uint8 result;
         
         assembly {
+            // index of the head pointer, within the page
             let index := mod(head, 32)
+
+            // shift the number to the right to unpack it from the page
             result := shr(mul(sub(0x1F, index), 8), page)
-            //page := shl(8, page)
+
+            // clear the number from the page
+            page := and(page, not(shl(mul(sub(0x1F, index), 8), 0xFF)))
         }
-        
-        //_queue.pages[pageIndex] = page;
+        _queue.pages[pageIndex] = page;
         unchecked {
             ++_queue.head;
             --_queue.count;
